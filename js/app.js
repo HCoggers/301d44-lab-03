bf5f52c360ff51d4c8eb02b6c1e7e1ff5111df61
abferris <abferris@gmail.com>
2019-02-14 11:53:35 -0800

    feature 2 donezo

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ function Horn(horn) {
   this.horns = horn.horns;
 }
 
+const hornKeys= [];
 Horn.allHorns = [];
 
 Horn.prototype.render = function(idx) {
-  $('main').append(`<div class="clone" id="Horn${idx}"></div>`);
+  $('main').append(`<div class="${this.class}" id="Horn${idx}"></div>`);
   let hornClone = $(`#Horn${idx}`);
 
   let hornHtml = $('#photo-template').html();
@@ -23,6 +24,10 @@ Horn.prototype.render = function(idx) {
   hornClone.find('img').attr('class', this.class);
   hornClone.find('img').attr('alt', this.alt);
   hornClone.find('p').text(`Number of of horns: ${this.horns}`);
+  if (!hornKeys.includes(this.class)){
+    hornKeys.push(this.class);
+    $('select').append(`<option value="${this.class}"> ${this.class} </option>`);
+  };
 }
 
 Horn.readJson = () => {
@@ -38,3 +43,17 @@ Horn.readJson = () => {
 Horn.loadHorns = () => Horn.allHorns.forEach( (horn, idx) => horn.render(idx));
 
 $(() => Horn.readJson());
+
+$('select').on('change', (selection) => {
+  console.log($('select :selected').val());
+  const pictures = $('div').get();
+  console.log('horned pictures', pictures);
+  $('div').hide();
+  pictures.forEach( val => {
+    if(val.className === $('select :selected').val()){
+      console.log($(`div .${val.className}`));
+
+       $(`div.${val.className}`).show();
+    };
+  });
+});
