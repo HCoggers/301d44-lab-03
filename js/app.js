71f9dfba24ca5946729c61b15019b7b23c565571
HCoggers <harry.cogswell@gmail.com>
2019-02-18 09:32:42 -0800

    refactors for handlebars functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const hornTemplate = $('#photo-template').html();
+console.log(hornTemplate);
+
 function Horn(horn) {
   this.name = horn.title;
   this.imgurl = horn.image_url;
@@ -15,19 +18,14 @@ Horn.prototype.render = function(idx) {
   $('main').append(`<div class="${this.class}" id="Horn${idx}"></div>`);
   let hornClone = $(`#Horn${idx}`);
 
-  let hornHtml = $('#photo-template').html();
+  const compiled = Handlebars.compile(hornTemplate);
+  hornClone.html(compiled(this));
 
-  hornClone.html(hornHtml);
-
-  hornClone.find('h2').text(this.name);
-  hornClone.find('img').attr('src', this.imgurl);
-  hornClone.find('img').attr('class', this.class);
-  hornClone.find('img').attr('alt', this.alt);
-  hornClone.find('p').text(`Number of of horns: ${this.horns}`);
   if (!hornKeys.includes(this.class)){
     hornKeys.push(this.class);
     $('select').append(`<option value="${this.class}"> ${this.class} </option>`);
   };
+
 }
 
 Horn.readJson = () => {
@@ -47,12 +45,12 @@ Horn.readJson = () => {
 }
 
 $('#gal1').on('click', () => {
-  console.log('gal2');
+  console.log('gal1');
   runSwitch(1);
 })
 
 $('#gal2').on('click', () => {
-  console.log('gal1');
+  console.log('gal2');
   runSwitch(2);
 })
 
@@ -64,13 +62,11 @@ const runSwitch = (gallery) => {
     case 1: 
     for(let i = 0; i < 20; i++){
       Horn.allHorns[i].render(i);
-      console.log('case 1');
     }
     break;
     case 2:
     for(let i = 20; i < 40; i++){
       Horn.allHorns[i].render(i);
-      console.log('case 2');
     }
   };
 }
@@ -82,14 +78,12 @@ Horn.loadHorns = () => {
 
 $(() =>   Horn.readJson());
 
-$('select').on('change', (selection) => {
+$('select').on('change', () => {
   console.log($('select :selected').val());
   const pictures = $('div').get();
-  console.log('horned pictures', pictures);
   $('div').hide();
   pictures.forEach( val => {
     if(val.className === $('select :selected').val()){
-      console.log($(`div .${val.className}`));
        $(`div.${val.className}`).show();
     };
   });
